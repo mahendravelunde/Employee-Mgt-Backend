525bb7a0b78b210e7b058901d078eee69307c443
Mahendra Velunde <mahendra.velundealphawarenext@gmail.com>
2025-03-15 17:44:34 +0530

    deployed code

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+            useNewUrlParser: true,// old 3.6 engine has issue with special charachter
+            useUnifiedTopology: true,// engine prevent deprecation waring 
         });
         console.log('MongoDB connected');
     } catch (err) {
